64d4725d6971cf33a8f93e26ac8c5fdf7a549aa0
Namit Saxena <namit2saxena@gmail.com>
2020-07-20 22:27:56 +0530

    Table names

--- NewsFeed/models.py
+++ NewsFeed/models.py
@@ -10,6 +10,7 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -34,12 +35,13 @@ class User(db.Model, UserMixin):
      return f"User('{self.username}', '{self.email}', '{self.image_file}')"
      
 class Post(db.Model):
+    __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     link = db.Column(db.String(200), nullable = False)
     website = db.Column(db.String(40), nullable = False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     def __repr__(self):
          return f"User('{self.title}', '{self.date_posted}', '{self.content}', '{self.link}', '{self.website}')"

13a701cfb74318149690c89d2b64c027a25f33a7
Namit Saxena <namit2saxena@gmail.com>
2020-07-20 22:34:14 +0530

    Changing table name

--- NewsFeed/models.py
+++ NewsFeed/models.py
@@ -35,7 +35,6 @@ class User(db.Model, UserMixin):
      return f"User('{self.username}', '{self.email}', '{self.image_file}')"
      
 class Post(db.Model):
-    __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)

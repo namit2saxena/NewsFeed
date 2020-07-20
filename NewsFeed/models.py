9395e7871af0b183fc312b57a85bcc6b34f0d31d
Namit Saxena <namit2saxena@gmail.com>
2020-07-20 22:49:22 +0530

    Foreign key for Users

--- NewsFeed/models.py
+++ NewsFeed/models.py
@@ -16,7 +16,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default = 'default.jpeg')
     password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
+    posts = db.relationship('Post', db.ForeignKey('post.id') backref='author', lazy=True)
 
     def get_reset_token(self, expires_sec=1800):
      s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
@@ -41,6 +41,6 @@ class Post(db.Model):
     content = db.Column(db.Text, nullable=False)
     link = db.Column(db.String(200), nullable = False)
     website = db.Column(db.String(40), nullable = False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     def __repr__(self):
          return f"User('{self.title}', '{self.date_posted}', '{self.content}', '{self.link}', '{self.website}')"

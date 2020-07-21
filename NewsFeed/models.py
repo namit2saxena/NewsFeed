6a8ed074aa438551053279e85eea28baf61142e7
Namit Saxena <namit2saxena@gmail.com>
2020-07-21 08:45:56 +0530

    Added primary join

--- NewsFeed/models.py
+++ NewsFeed/models.py
@@ -15,7 +15,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default = 'default.jpeg')
     password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
+    posts = db.relationship('Post', backref='author', lazy=True, primaryjoin="User.id == Post.user_id")
 
     def get_reset_token(self, expires_sec=1800):
      s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
@@ -35,7 +35,7 @@ class User(db.Model, UserMixin):
      
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(100), nullable=False)   
     date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     link = db.Column(db.String(200), nullable = False)
